5a7599e6dad9a84952d81c063deac90495f91cd4
SUN <sunxiang0918@gmail.com>
2016-02-19 10:57:33 +0800

    增加了IOS版本的framework

---
+++ zkClient4Swift-Demo-iOS/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  zkClient4Swift-Demo-iOS
+//
+//  Created by SUN on 16/2/19.
+//  Copyright © 2016年 SUN. All rights reserved.
+//
+
+import UIKit
+import zkClient4Swift
+
+class ViewController: UIViewController {
+
+    let zkClient = ZkClient(serverstring: "127.0.0.1:2181")
+    
+    @IBAction func connection(sender: UIButton) {
+        zkClient.connect()
+        
+        debugPrint(zkClient.connected)
+        
+        //        let children = zkClient.getChildren("/ftengine")
+        //
+        //        for child in  children! {
+        //            print("子节点:\(child)")
+        //        }
+        
+        //        print("接收到的数据:\(zkClient.readData("/Hello/Byte",watch: true))")
+        
+        zkClient.subscribeChildChanges("/Hello", listenerName: "HelloChildChanges") { (path, children) -> Void in
+            print("路径:\(path)子节点发生变化:")
+            if let cc  = children {
+                for c in cc {
+                    print("\(c)")
+                }
+            }
+        }
+        
+        zkClient.subscribeDataChanges("/Hello/Byte", listenerName: "ByteNodeDataChanges") { (path, data) -> Void in
+            print("路径:\(path)节点内容发生变化,新的内容为:\(data)")
+        }
+        
+        zkClient.subscribeDataDelete("/Hello/Byte", listenerName: "ByteNodeDelete") { (path) -> Void in
+            print("路径节点:\(path)被删除")
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ zkClient4Swift_iOS/zkClient4Swift_iOS.h
@@ -0,0 +1,19 @@
+//
+//  zkClient4Swift_iOS.h
+//  zkClient4Swift_iOS
+//
+//  Created by SUN on 16/2/19.
+//  Copyright © 2016年 SUN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for zkClient4Swift_iOS.
+FOUNDATION_EXPORT double zkClient4Swift_iOSVersionNumber;
+
+//! Project version string for zkClient4Swift_iOS.
+FOUNDATION_EXPORT const unsigned char zkClient4Swift_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <zkClient4Swift_iOS/PublicHeader.h>
+
+
